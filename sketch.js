e60b46a0419fe4767487e918296bf6acd7c4827b
Greg K <gkreisman_cep@qq.com>
2024-05-14 16:26:25 +0800

    fix spacing

--- sketch.js
+++ sketch.js
@@ -2,10 +2,14 @@ let img
 let cnv
 let button
 let output
+let cx,cy
 
 
 function setup() {
 cnv =createCanvas(800, 830);
+ cx = floor((windowWidth - cnv.width) / 2);
+   cy = floor((windowHeight - cnv.height) / 2);
+  cnv.position(cx, cy);
   getStreet()
   background(128)
   textSize(30);
@@ -30,7 +34,8 @@ cnv =createCanvas(800, 830);
 async function getStreet(){
    
   img = await createImg("https://image.pollinations.ai/prompt/city%20sidwalk%20bricks%20painted%20with%20warning%20%20in%20chinese%20do%20not%20park%20here%20and%20a%20stencil%20image%20of%20of%20a%20bicycle?width=800&height=800&nologo=true&seed="+floor(random(255)),"street scene")
- img.position(cnv.position.x,cnv.position.y)
+ img.position(cx,cy)
  button.html("click for new image")
+ //cnv.image(img,0,0)
    
 }
