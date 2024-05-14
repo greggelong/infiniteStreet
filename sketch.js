d077bd8ea88d3b8c039db0dbf9ab97515d6ce9b1
Greg K <gkreisman_cep@qq.com>
2024-05-14 13:50:10 +0800

    add color bar

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 let img
 let cnv
 let button
+let output
+
 
 function setup() {
-cnv =createCanvas(800, 800);
+cnv =createCanvas(800, 830);
   getStreet()
   background(128)
   textSize(30);
@@ -12,14 +14,23 @@ cnv =createCanvas(800, 800);
   // Display the text.
   text('Wait for image', width/2, 10, 100);
   button = createButton("Click for new image")
-  button.mousePressed(getStreet)
+  button.mousePressed(doit)
   button.position(cnv.width/2,cnv.height+50)
   button.style('font-size', '30px');
+  //output.position(button.x,button.y+60)
 }
 
- 
+ async function doit(){
+    background(random(255),random(255),random(255))
+    await getStreet()
+   
+
+ }
 
 async function getStreet(){
+   
   img = await createImg("https://image.pollinations.ai/prompt/city%20sidwalk%20bricks%20painted%20with%20warning%20%20in%20chinese%20do%20not%20park%20here%20and%20a%20stencil%20image%20of%20of%20a%20bicycle?width=800&height=800&nologo=true&seed="+floor(random(255)),"street scene")
  img.position(cnv.position.x,cnv.position.y)
+ button.html("click for new image")
+   
 }
